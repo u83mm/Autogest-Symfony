d213d67473fce08cf78ce5bebc52760c1ce17395
Mario Moreno <cursotecnoweb@gmail.com>
2025-12-15 18:52:23 +0100

    feat: update 'municipios.js' javaScript file

--- public_html/js/municipios.js
+++ public_html/js/municipios.js
@@ -3,27 +3,22 @@
 // Muestra municipios a través del código postal mediante un JSON
 let cp = document.getElementById("cliente_codigoPostal");
 
-if(cp) {		
-    cp.addEventListener("blur", () => {
+if(cp) {    	
+    cp.addEventListener("blur", () => {        
         fetch("/municipios.json")
             .then(response => response.json())
-            .then(data => {
-                const cp = document.getElementById("cliente_codigoPostal").value;        
-                
-                data.forEach(element => {                       
-                    try {
-                        if(element.cp == cp) {                             
-                            document.getElementById("cliente_localidad").value = element.municipio;
-                            document.getElementById("cliente_provincia").value = element.provincia;                                      
-                        }
-                        else {
-                            throw new Error("No se ha encontrado");                            
-                        }
-                    } catch (error) {
-                        document.getElementById("cliente_localidad").value = error.message;
-                        document.getElementById("cliente_provincia").value = error.message;
-                    }       
-                });                
-            })
+            .then(data => {                                
+                for (let i = 0; i < data.length; i++) {
+                    if(data[i].cp == cp.value) {
+                        document.getElementById("cliente_localidad").value = data[i].municipio;
+                        document.getElementById("cliente_provincia").value = data[i].provincia;
+                        break;
+                    }
+                    else {
+                        document.getElementById("cliente_localidad").value = "No se ha encontrado";
+                        document.getElementById("cliente_provincia").value = "No se ha encontrado";
+                    }
+                }
+            });
     });
 }
